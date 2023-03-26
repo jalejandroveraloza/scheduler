b73af97ba8c265932bf59bfbafee50c1d56911be
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-03-26 15:07:20 -0700

    adding daylist component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 
+
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
 
   const days = [
     {
@@ -35,8 +38,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
