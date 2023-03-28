c360eba2a8a6ab2265516995efe28941014c6fb7
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-03-27 20:30:05 -0700

    implementing the appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classNames from "classnames"
+
+import "components/Appointment/styles.scss"
+
+export default function Appointment(){
+  return (
+    <article className="appointment"></article>
+  )
+}
