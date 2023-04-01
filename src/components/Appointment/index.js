57142477e047c48271539c478f29aab7210584ac
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-03-31 20:34:45 -0700

    implementing displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,8 +3,15 @@ import classNames from "classnames"
 
 import "components/Appointment/styles.scss"
 
-export default function Appointment(){
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+export default function Appointment(props){
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/>: <Empty />}
+    </article>
   )
 }
