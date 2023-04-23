1bc5bdc48950548d810479f4d2238438cf3a3cfd
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-04-23 13:21:02 -0700

    type checking with prop types

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -131,7 +131,7 @@ export default function useApplicationData() {
       data:appointment
     }
     return axios(req).then(response =>{
-      console.log("response from delete axios===>",response);
+      //console.log("response from delete axios===>",response);
       setState({...state, appointments, days});
     })
 
